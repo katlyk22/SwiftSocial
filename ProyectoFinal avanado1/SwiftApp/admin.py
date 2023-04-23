f3d0749d58ce49737b78469a6628a569e77b2d1e
katlyk22 <90731835+katlyk22@users.noreply.github.com>
2023-04-22 21:13:37 -0300

    Delete ProyectoFinal avanado1 directory

--- ProyectoFinal avanado1/SwiftApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from SwiftApp.models import Post
-
-admin.site.register(Post)
-
--- ProyectoFinal avanado1/SwiftApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SwiftappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SwiftApp'
